12031c3b4bb67bad5a800f43aed9d9b9feac4bb8
Krzysztof Owsiany <gocom@o2.pl>
2020-03-16 23:33:11 +0100

    test: start work at view tests

---
+++ Sound/Sound.Tests/work_stopped_time_view_tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GWTTestBase;
+using Sound.Infrastructure;
+using Sound.Application.Events;
+using Sound.Application.Views;
+using Xunit;
+
+namespace Sound.Tests
+{
+    public class work_stopped_time_view_tests : TestBase
+    {
+        [Fact]
+        public void when_work_stopped_then_sound_stopped()
+        {
+            Give(@event: new WorkStopped(25, 
+                DateTime.Parse("2019-01-01 23:25"), 
+                DateTime.Parse("2019-01-01 23:25")));
+
+            //TODO: implement test base for views
+            Then(new WorkStopTime(DateTime.Parse("2019-01-01 23:25")));
+        }
+    }
+}
