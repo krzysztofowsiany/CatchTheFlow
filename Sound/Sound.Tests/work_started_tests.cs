48a0e1c1312ed9d05594e2aee83db239d4bf0189
Krzysztof Owsiany <gocom@o2.pl>
2020-03-19 00:13:52 +0100

    test: add work started test

---
+++ Sound/Sound.Tests/work_started_tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using GWTTestBase;
+using Sound.Infrastructure;
+using Sound.Application.Events;
+using Xunit;
+
+namespace Sound.Tests
+{
+    public class work_started_tests : TestBase
+    {
+        [Fact]
+        public void when_work_started_then_sound_should_started()
+        {
+            Give(@event: new WorkSoundUpdated(
+                "work_1.mp3",
+                DateTime.Parse("2019-01-01 23:25")));
+            
+            Give(@event: new WorkStarted(25, 
+                DateTime.Parse("2019-01-01 23:25"), 
+                DateTime.Parse("2019-01-01 23:25")));
+
+            When<EventListener>();
+
+            Then(new SoundStarted(
+                "work_1.mp3",
+                DateTime.Parse("2019-01-01 23:25")));
+        }
+    }
+}
