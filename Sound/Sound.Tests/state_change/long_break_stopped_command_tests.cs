fdfc86ced034c7b24f2d7593f1bbbfdfe6d1c579
Krzysztof Owsiany <gocom@o2.pl>
2020-08-07 01:34:09 +0200

    afix: long breake tests

---
+++ Sound/Sound.Tests/state_change/long_break_stopped_command_tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GWTTestBase;
+using Sound.Application.Commands;
+using Sound.Application.Events;
+using Xunit;
+
+namespace Sound.Tests.state_change
+{
+    public class long_break_stopped_command_tests : CommandTest<sound_module, StopPlayCommand>
+    {
+        [Fact]
+        public void when_long_breake_stopped_then_sound_stopped()
+        {
+            When(new StopPlayCommand(
+                DateTime.Parse("2019-01-01 23:45"))
+            );
+
+            Then(new SoundStopped());
+        }
+    }
+}
